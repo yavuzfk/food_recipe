5dd107f22ae150e6c1e3615ae4ad969919d2e730
yavuzfk <yavuzfk@hotmail.com>
2023-08-01 09:04:23 +0300

    profile page

---
+++ lib/screens/profile_page.dart/profile_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile Page'),
+        ),
+        body: const Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Profile Page',
+            ),
+          ],
+        )));
+  }
+}
